7386b83c3459e592d93d19c5bce6c09a9a48efee
Gustavo Alves <gustavodossantos1704@gmail.com>
2022-11-19 08:36:13 -0300

    :sparkles: feat: advinhando 4

--- advinhando_3.py
+++ advinhando_3.py
@@ -1,3 +1,18 @@
 print('Seja bem-vindo')
-def resultado (a, b):
 
+
+def resultado(a, b):
+    if a == b:
+        return 'VOCÊ VENCEU'
+    else:
+        if a > b:
+            return 'ALTO'
+        else:
+            return 'BAIXO'
+
+
+num = 0
+num_sec = 33
+while num != 33:
+    num = int(input('Digite um número: '))
+    print(resultado(num, num_sec))
