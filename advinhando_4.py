a1119e25567d4125ed92ed266a42e137a0688300
Gustavo Alves <gustavodossantos1704@gmail.com>
2022-11-19 08:50:17 -0300

    :sparkles: add funcionalidade advinhando_4

--- advinhando_4.py
+++ advinhando_4.py
@@ -1,1 +1,21 @@
+from random import randint
+
+
+def resultado(a, b):
+    if a == b:
+        return 'VOCÊ VENCEU'
+    else:
+        if a > b:
+            return 'ALTO'
+        else:
+            return 'BAIXO'
+
+
 print('Seja bem-vindo')
+
+num_sec = randint(1, 100)
+num = 0
+while num != num_sec:
+    num = int(input('Digite um número: '))
+    print(resultado(num, num_sec))
+print('fim de jogo')
